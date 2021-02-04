c8e086229490925776bc87d876998904e0122f05
poorvijhunjhunwala <78332284+poorvijhunjhunwala@users.noreply.github.com>
2021-02-04 12:24:28 +0530

    Update index.js

--- message/js/index.js
+++ message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "You are the best friend whome I cherrish a lot. It would not be false if I say, we have grown together, as people knowing almost everything about the other. I always strive to make everyday happy for you, but mostly I urge to live all those together with you. I may not be the perfect person to look up to, I will always stand by your side and be there for you, as a friend and as a person who understands you. I wish you all the love and success in all your endeavors.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "You are the best friend whome I cherrish a lot. It would not be false if I say, we have grown together, two people knowing almost everything about each other. Nothing in my life is complete without sharing it with you, but mostly I long to live and make all my memories together with you. I may not be the perfect person to look up to, But, I will always stand by your side and be there for you, as a friend and as a person who understands you. I wish you all the love and success in all your endeavors.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
