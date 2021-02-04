62d64d39daa13e4bb955dcd6dc327bb074e0fd64
poorvijhunjhunwala <78332284+poorvijhunjhunwala@users.noreply.github.com>
2021-02-04 12:03:22 +0530

    Update index.js

--- message/js/index.js
+++ message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "You are the best friend I never askedd for. It would not be false if I say, we have grown together. Both as people and as knowing what we want in life. I always strive to make everyday happy for you, but mostly I urge to live all those together with you. I may not be the perfect person to look up to, but I tell you Poorvi, even if I cannot help you in any way, I will always stand by your side, as a friend as a person close to you. I wish you all the love and success in all your endeavors.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "You are the best friend I never asked for. It would not be false if I say, we have grown together, as people knowing almost everything about the other. I always strive to make everyday happy for you, but mostly I urge to live all those together with you. I may not be the perfect person to look up to, I will always stand by your side andd be there for you, as a friend and as a person who understands you. I wish you all the love and success in all your endeavors.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
